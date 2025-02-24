26db45fd3ac3dc5bf3ace7cf4e63af77ed28fbe0
Max Reis <maxreis507@gmail.com>
2025-02-24 19:11:27 +0100

    Update: add createAccount method to ClientController

--- src/main/java/controllers/ClientController.java
+++ src/main/java/controllers/ClientController.java
@@ -1,6 +1,26 @@
 package controllers;
 
+import models.Client;
+import models.ClientDAO;
 
+import java.sql.SQLException;
 
 public class ClientController {
+    private ClientDAO clientDAO;
+
+    public ClientController(ClientDAO clientDAO) {
+        this.clientDAO = clientDAO;
+    };
+
+    public void createAccount(String name, String password, double initialBalance) {
+        Client client = new Client(name, password, initialBalance);
+
+        try {
+            clientDAO.addClient(client);
+        } catch (SQLException e) {
+            System.out.println("Account creation failed " + e.getMessage());
+        }
+
+        System.out.println("Account created successfully!");
+    }
 }
