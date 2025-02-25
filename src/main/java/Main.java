0fc1743b155d02a9bf2e31012e28b7132261fc74
Max Reis <maxreis507@gmail.com>
2025-02-25 10:47:32 +0100

    Fix: add database connection to app start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,18 @@
 import controllers.ClientController;
 import models.ClientDAO;
+import models.DatabaseConnection;
+import models.DatabaseSetup;
 import views.CLIView;
 
 public class Main {
     public static void main(String[] args) {
+        DatabaseConnection.connect();       // Connect to sqlite database
+        DatabaseSetup.createTables();       // Create tables
+
         ClientDAO clientDAO = new ClientDAO();
         ClientController clientController = new ClientController(clientDAO);
-        CLIView cli = new CLIView(clientController);
 
+        CLIView cli = new CLIView(clientController);
         cli.displayMainMenu();
     }
 }
